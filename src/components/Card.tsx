8e30f6da3f3804597a4bbda500309074238e71c5
Samy572 <samy.chabbi@gmx.fr>
2023-03-28 16:09:49 +0200

    ajout des filtres de recherche

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,11 +2,11 @@ const Card = ({ country }) => {
 	return (
 		<li className="card">
 			<img src={country.flags.svg} alt={'drapeau' + country.translations.fra.common} />
-			<div className="infos">
+			<a href={country.maps.googleMaps} target="_blank" className="infos">
 				<h2>{country.translations.fra.common}</h2>
 				<h4>{country.capital}</h4>
 				<p>Pop. {country.population.toLocaleString()}</p>
-			</div>
+			</a>
 		</li>
 	);
 };
